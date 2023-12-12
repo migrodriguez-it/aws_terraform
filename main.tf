4be4cea66b9dff4bde6843b879930fc375d256d5
Miguel Rodriguez <miguel@silicio14.co>
2023-12-12 12:08:23 -0500

    main

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Provider configuration
+provider "aws" {
+    region = "us-west-2"
+}
+
+# Resource definition
+resource "aws_instance" "example" {
+    ami           = "ami-0c94855ba95c71c99"
+    instance_type = "t2.micro"
+}
